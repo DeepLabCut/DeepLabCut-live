80b235bb10ec65027906d3fca199000d652d386b
AlexEMG <alexander@deeplabcut.org>
2020-08-03 15:29:12 -0400

    pipy sync

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setuptools.setup(
     name="deeplabcut-live",
-    version="0.0.1",
+    version="0.0",
     author="A. & M. Mathis Labs",
     author_email="admin@deeplabcut.org",
     description="Class to load exported DeepLabCut networks and perform pose estimation on single frames (from a camera feed)",
