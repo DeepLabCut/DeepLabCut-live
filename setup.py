c716cb102300070ceb1b59e082652eeddcbee2dc
AlexEMG <alexander@deeplabcut.org>
2021-05-15 19:43:36 +0200

    v1.0.1; minor bug fixes

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 setuptools.setup(
     name="deeplabcut-live",
-    version="1.0",
+    version="1.0.1",
     author="A. & M. Mathis Labs",
     author_email="admin@deeplabcut.org",
     description="Class to load exported DeepLabCut networks and perform pose estimation on single frames (from a camera feed)",
