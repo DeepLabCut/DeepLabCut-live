cadf6475deeabcaf3ab39c4ba98b5d8c3f89390a
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 20:22:49 +0000

    Adding numeric_owner as keyword arguement

--- dlclive/benchmark.py
+++ dlclive/benchmark.py
@@ -87,7 +87,7 @@ def download_benchmarking_data(
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, target_dir, members=tarfilenamecutting(tar))
