96ef85cdf26078ac636d07a1d1b584027989fc68
Alexander Mathis <mathisepfl@gmail.com>
2023-05-05 19:44:46 +0200

    Add files via upload

---
+++ version.py
@@ -0,0 +1,11 @@
+"""
+DeepLabCut Live Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+admin@deeplabcut.org
+
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+
+__version__ = "1.0.4"
+VERSION = __version__
