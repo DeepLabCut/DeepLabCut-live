770321dcc25c10a5c0168c42330d186071f4bd18
Jaap de Ruyter <deruyter92@gmail.com>
2026-01-14 09:26:38 +0100

    Update pyproject.toml
    - pyproject.toml is made PEP 517 / 518 / 621–compliant, which makes it easy to inherit dependencies
    - it is more tool-agnostic (no poetry required & facilitates uv packaging).
    - Python versions 3.10 up to 3.12
    - tensorflow installation is restricted to python < 3.12 for windows

--- reinstall.sh
+++
@@ -1,5 +0,0 @@
-poetry shell          # activating current environment
-poetry install        # creating and installing current project
-poetry build          # creating the tarball
-poetry publish        # uploading to pypi
-#poetry publish --username=<username> --password=<password>
